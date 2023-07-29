f1dc3b760e6d1a19e6dce054ee418dd3a829b99e
hacker1024 <hacker1024@users.sourceforge.net>
2023-07-29 21:24:24 +1000

    Remove the buildROSWorkspace devPackagePredicate API

--- README.md
+++ README.md
@@ -28,14 +28,14 @@ issues.
 examples are designed to be invoked with [`callPackage`](https://nixos.org/guides/nix-pills/callpackage-design-pattern.html), e.g.
 `rosPackages.rolling.callPackage`.
 
-`buildROSWorkspace.explicit` takes a derivation name and two lists of packages.
+`buildROSWorkspace` takes a derivation name and two lists of packages.
 
 `devPackages` are packages that are under active development. They will be
 available in the release environment (`nix-build`), but in the development
 environment (`nix-shell`), only the build inputs of the packages will be
 available.
 
-`extraPackages` are packages that are not under active development (typically
+`prebuiltPackages` are packages that are not under active development (typically
 third-party packages). They will be available in both the release and
 development environments.
 
@@ -55,28 +55,6 @@ buildROSWorkspace.explicit {
   extraPackages = [
     rviz2
   ];
-}
-```
-
-For finer control over package classification, `buildROSWorkspace` can be used
-directly. For example, if the `devPackages` had a `passthru` attribute indicating their
-status:
-
-```nix
-{ buildROSWorkspace
-, rviz2
-, my-package-1
-, my-package-2
-}:
-
-buildROSWorkspace {
-  name = "my";
-  packages = [
-    my-package-1
-    my-package-2
-    rviz2
-  ];
-  devPackagePredicate = pkg: pkg.devPackage or false;
 }
 ```
 
--- packages/ros/build-ros-workspace/default.nix
+++ packages/ros/build-ros-workspace/default.nix
@@ -15,133 +15,116 @@ let
     argcomplete;
 in
 
+{
+  # The name of the workspace.
+  name
+
+  # Configure the workspace for interactive use.
+, interactive ? true
+
+, devPackages ? [ ]
+, prebuiltPackages ? [ ]
+}:
+
 let
-  buildROSWorkspace =
-    {
-      # The name of the workspace.
-      name
-
-      # Configure the workspace for interactive use.
-    , interactive ? true
-
-    , packages ? [ ]
-    , devPackagePredicate ? pkg: true
-    }:
-
-    let
-      # Include standard packages in the workspace.
-      standardPackages = [
-        ros-core
-      ] ++ lib.optionals interactive ([
-        (writeShellScriptBin "mk-workspace-shell-setup"
-          # The shell setup script is designed to be sourced.
-          # By appearing to generate the script dynamically, this pattern is
-          # enforced, as there is no file that can be executed by mistake.
-          "cat ${substituteAll {
+  # Include standard packages in the workspace.
+  standardPackages = [
+    ros-core
+  ] ++ lib.optionals interactive ([
+    (writeShellScriptBin "mk-workspace-shell-setup"
+      # The shell setup script is designed to be sourced.
+      # By appearing to generate the script dynamically, this pattern is
+      # enforced, as there is no file that can be executed by mistake.
+      "cat ${substituteAll {
             name = "workspace-shell-setup.sh";
             src = ./shell_setup.sh;
             inherit argcomplete;
           }}")
-      ]);
+  ]);
 
-      ###
-      # Sort packages into various categories.
-      splitDevPackages = builtins.partition devPackagePredicate packages;
-      devPackages = splitDevPackages.right;
-      prebuiltPackages = standardPackages ++ splitDevPackages.wrong;
+  # Sort packages into various categories.
+  splitRosPackages = builtins.partition (pkg: pkg.rosPackage or false) (standardPackages ++ devPackages ++ prebuiltPackages);
+  rosPackages = splitRosPackages.right;
+  otherPackages = splitRosPackages.wrong;
 
-      splitRosPackages = builtins.partition (pkg: pkg.rosPackage or false) (standardPackages ++ packages);
-      rosPackages = splitRosPackages.right;
-      otherPackages = splitRosPackages.wrong;
+  splitRosPrebuiltPackages = builtins.partition (pkg: pkg.rosPackage or false) (standardPackages ++ prebuiltPackages);
+  rosPrebuiltPackages = splitRosPrebuiltPackages.right;
+  otherPrebuiltPackages = splitRosPrebuiltPackages.wrong;
 
-      splitRosPrebuiltPackages = builtins.partition (pkg: pkg.rosPackage or false) prebuiltPackages;
-      rosPrebuiltPackages = splitRosPrebuiltPackages.right;
-      otherPrebuiltPackages = splitRosPrebuiltPackages.wrong;
-      ###
+  # The ROS overlay's buildEnv has special logic to wrap ROS packages so that
+  # they can find each other.
+  # Unlike the regular buildEnv from Nixpkgs, however, it is designed only with
+  # nix-shell in mind, and propagates non-ROS packages rather than including
+  # them properly.
+  # We must use a combination of the ROS buildEnv and Nixpkgs buildEnv to
+  # include all packages in the environment.
+  workspace = buildEnv {
+    name = "${name}-workspace";
+    paths = [
+      (buildROSEnv' { paths = rosPackages; })
+    ] ++ otherPackages;
+    passthru = {
+      inherit
+        env
+        standardPackages
+        devPackages prebuiltPackages
+        rosPackages otherPackages;
+    };
+  };
 
-      # The ROS overlay's buildEnv has special logic to wrap ROS packages so that
-      # they can find each other.
-      # Unlike the regular buildEnv from Nixpkgs, however, it is designed only with
-      # nix-shell in mind, and propagates non-ROS packages rather than including
-      # them properly.
-      # We must use a combination of the ROS buildEnv and Nixpkgs buildEnv to
-      # include all packages in the environment.
-      workspace = buildEnv {
-        name = "${name}-workspace";
-        paths = [
-          (buildROSEnv' { paths = rosPackages; })
-        ] ++ otherPackages;
-        passthru = {
-          inherit
-            env
-            devPackages prebuiltPackages
-            rosPackages otherPackages;
-        };
-      };
+  # The workspace shell environment includes non-dev packages as-is as well as
+  # build inputs of dev packages.
+  #
+  # This allows packages to be developed, built and tested with all tools
+  # and dependencies available.
+  env = mkShell {
+    name = "${name}-workspace-env";
 
-      # The workspace shell environment includes non-dev packages as-is as well as
-      # build inputs of dev packages.
-      #
-      # This allows packages to be developed, built and tested with all tools
-      # and dependencies available.
-      env = mkShell {
-        name = "${name}-workspace-env";
+    packages = otherPrebuiltPackages ++ [
+      (buildROSEnv' { paths = rosPrebuiltPackages; })
 
-        packages = otherPrebuiltPackages ++ [
-          (buildROSEnv' { paths = rosPrebuiltPackages; })
+      # Add colcon, for building packages.
+      # This is a build tool that wraps other build tools, as does Nix, so it is
+      # not needed normally in any of the ROS derivations and must be manually
+      # added here.
+      colcon
+    ];
 
-          # Add colcon, for building packages.
-          # This is a build tool that wraps other build tools, as does Nix, so it is
-          # not needed normally in any of the ROS derivations and must be manually
-          # added here.
-          colcon
-        ];
+    inputsFrom = devPackages;
 
-        inputsFrom = devPackages;
+    shellHook = ''
+      ${
+        # While the modified version of buildROSEnv contains fixes for ROS
+        # packages in the buildROSEnv environment, these do not apply to packages
+        # that are being developed and built outside of Nix.
+        # The environment must be configured here as well.
+        ''
+          export RMW_IMPLEMENTATION=rmw_fastrtps_dynamic_cpp
+        ''
+      }
 
-        shellHook = ''
-          ${
-            # While the modified version of buildROSEnv contains fixes for ROS
-            # packages in the buildROSEnv environment, these do not apply to packages
-            # that are being developed and built outside of Nix.
-            # The environment must be configured here as well.
-            ''
-              export RMW_IMPLEMENTATION=rmw_fastrtps_dynamic_cpp
-            ''
-          }
+      if [ -z "$NIX_EXECUTING_SHELL" ]; then
+        eval "$(mk-workspace-shell-setup)"
+      else
+        # If a different shell is in use through a tool like https://github.com/chisui/zsh-nix-shell,
+        # this hook will not be running in it. "mk-workspace-shell-setup" must be run manually.
+        if [ -z "$I_WILL_RUN_WORKSPACE_SHELL_SETUP" ]; then
+          echo >&2 'The shell setup script must be manually run.'
+          echo >&2 '$ eval "$(mk-workspace-shell-setup)"'
+          echo >&2 'Set I_WILL_RUN_WORKSPACE_SHELL_SETUP=1 to silence this message.'
+        fi
+      fi
+    '';
+  };
 
-          if [ -z "$NIX_EXECUTING_SHELL" ]; then
-            eval "$(mk-workspace-shell-setup)"
-          else
-            # If a different shell is in use through a tool like https://github.com/chisui/zsh-nix-shell,
-            # this hook will not be running in it. "mk-workspace-shell-setup" must be run manually.
-            if [ -z "$I_WILL_RUN_WORKSPACE_SHELL_SETUP" ]; then
-              echo >&2 'The shell setup script must be manually run.'
-              echo >&2 '$ eval "$(mk-workspace-shell-setup)"'
-              echo >&2 'Set I_WILL_RUN_WORKSPACE_SHELL_SETUP=1 to silence this message.'
-            fi
-          fi
-        '';
-      };
-
-      # A modified version of buildROSEnv that works around
-      # lopsided98/nix-ros-overlay#45.
-      buildROSEnv' = { paths, postBuild ? "", ... }@args: buildROSEnv (args // {
-        paths = paths ++ [ rmw-fastrtps-dynamic-cpp ];
-        postBuild = ''
-          ${postBuild}
-          rosWrapperArgs+=(--set-default RMW_IMPLEMENTATION rmw_fastrtps_dynamic_cpp)
-        '';
-      });
-    in
-    workspace;
-in
-{
-  __functor = self: buildROSWorkspace;
-
-  # A higher-level API over buildROSWorkspace that allows specific sets of development and extra packages to be specified.
-  explicit = { devPackages, extraPackages, ... }@args: buildROSWorkspace ((builtins.removeAttrs args [ "devPackages" "extraPackages" ]) // {
-    packages = extraPackages ++ map (pkg: pkg.overrideAttrs ({ passthru ? { }, ... }: { passthru = passthru // { devPackage = true; }; })) devPackages;
-    devPackagePredicate = pkg: pkg.devPackage or false;
+  # A modified version of buildROSEnv that works around
+  # lopsided98/nix-ros-overlay#45.
+  buildROSEnv' = { paths, postBuild ? "", ... }@args: buildROSEnv (args // {
+    paths = paths ++ [ rmw-fastrtps-dynamic-cpp ];
+    postBuild = ''
+      ${postBuild}
+      rosWrapperArgs+=(--set-default RMW_IMPLEMENTATION rmw_fastrtps_dynamic_cpp)
+    '';
   });
-}
+in
+workspace
