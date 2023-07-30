01557ceadefd806c9e5412f04f079ea8c8f170a7
hacker1024 <hacker1024@users.sourceforge.net>
2023-07-30 16:10:28 +1000

    buildROSWorkspace: Expose ROS environment information in passthru

--- packages/ros/build-ros-workspace/default.nix
+++ packages/ros/build-ros-workspace/default.nix
@@ -58,70 +58,82 @@ let
   # them properly.
   # We must use a combination of the ROS buildEnv and Nixpkgs buildEnv to
   # include all packages in the environment.
-  workspace = buildEnv {
-    name = "${name}-workspace";
-    paths = [
-      (buildROSEnv' { paths = rosPackages; })
-    ] ++ otherPackages;
-    passthru = {
-      inherit
-        env
-        standardPackages
-        devPackages prebuiltPackages
-        rosPackages otherPackages;
+  workspace =
+    let
+      rosEnv = buildROSEnv' { paths = rosPackages; };
+    in
+    buildEnv {
+      name = "${name}-workspace";
+      paths = [ rosEnv ] ++ otherPackages;
+      passthru = {
+        inherit
+          env
+          rosEnv
+          standardPackages
+          devPackages prebuiltPackages
+          rosPackages otherPackages;
+        inherit (ros-core) rosVersion rosDistro;
+      };
     };
-  };
 
   # The workspace shell environment includes non-dev packages as-is as well as
   # build inputs of dev packages.
   #
   # This allows packages to be developed, built and tested with all tools
   # and dependencies available.
-  env = mkShell {
-    name = "${name}-workspace-env";
+  env =
+    let
+      rosEnv = buildROSEnv' { paths = rosPrebuiltPackages; };
+    in
+    mkShell {
+      name = "${name}-workspace-env";
 
-    packages = otherPrebuiltPackages ++ [
-      (buildROSEnv' { paths = rosPrebuiltPackages; })
+      packages = otherPrebuiltPackages ++ [
+        rosEnv
 
-      # Add colcon, for building packages.
-      # This is a build tool that wraps other build tools, as does Nix, so it is
-      # not needed normally in any of the ROS derivations and must be manually
-      # added here.
-      colcon
-    ];
+        # Add colcon, for building packages.
+        # This is a build tool that wraps other build tools, as does Nix, so it is
+        # not needed normally in any of the ROS derivations and must be manually
+        # added here.
+        colcon
+      ];
 
-    inputsFrom = devPackages;
+      inputsFrom = devPackages;
 
-    shellHook = ''
-      ${
-        # While the modified version of buildROSEnv contains fixes for ROS
-        # packages in the buildROSEnv environment, these do not apply to packages
-        # that are being developed and built outside of Nix.
-        # The environment must be configured here as well.
-        ''
-          export RMW_IMPLEMENTATION=rmw_fastrtps_dynamic_cpp
-        ''
-      }
+      passthru = {
+        inherit rosEnv;
+      };
 
-      # Explicitly set the Python executable used by colcon.
-      # By default, colcon will attempt to use the Python executable known at
-      # configure time, which does not make much sense in a Nix environment -
-      # if the Python derivation hash changes, the old one will still be used.
-      export COLCON_PYTHON_EXECUTABLE="${python}/bin/python"
+      shellHook = ''
+        ${
+          # While the modified version of buildROSEnv contains fixes for ROS
+          # packages in the buildROSEnv environment, these do not apply to packages
+          # that are being developed and built outside of Nix.
+          # The environment must be configured here as well.
+          ''
+            export RMW_IMPLEMENTATION=rmw_fastrtps_dynamic_cpp
+          ''
+        }
 
-      if [ -z "$NIX_EXECUTING_SHELL" ]; then
-        eval "$(mk-workspace-shell-setup)"
-      else
-        # If a different shell is in use through a tool like https://github.com/chisui/zsh-nix-shell,
-        # this hook will not be running in it. "mk-workspace-shell-setup" must be run manually.
-        if [ -z "$I_WILL_RUN_WORKSPACE_SHELL_SETUP" ]; then
-          echo >&2 'The shell setup script must be manually run.'
-          echo >&2 '$ eval "$(mk-workspace-shell-setup)"'
-          echo >&2 'Set I_WILL_RUN_WORKSPACE_SHELL_SETUP=1 to silence this message.'
+        # Explicitly set the Python executable used by colcon.
+        # By default, colcon will attempt to use the Python executable known at
+        # configure time, which does not make much sense in a Nix environment -
+        # if the Python derivation hash changes, the old one will still be used.
+        export COLCON_PYTHON_EXECUTABLE="${python}/bin/python"
+
+        if [ -z "$NIX_EXECUTING_SHELL" ]; then
+          eval "$(mk-workspace-shell-setup)"
+        else
+          # If a different shell is in use through a tool like https://github.com/chisui/zsh-nix-shell,
+          # this hook will not be running in it. "mk-workspace-shell-setup" must be run manually.
+          if [ -z "$I_WILL_RUN_WORKSPACE_SHELL_SETUP" ]; then
+            echo >&2 'The shell setup script must be manually run.'
+            echo >&2 '$ eval "$(mk-workspace-shell-setup)"'
+            echo >&2 'Set I_WILL_RUN_WORKSPACE_SHELL_SETUP=1 to silence this message.'
+          fi
         fi
-      fi
-    '';
-  };
+      '';
+    };
 
   # A modified version of buildROSEnv that works around
   # lopsided98/nix-ros-overlay#45.
